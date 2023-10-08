327aea12cf83827f600d6cd56e7ea0912a3cc533
Matheus Fernandes Santos <matheusfsantos.ti@gmail.com>
2023-10-08 02:39:18 -0300

    Realizando alterações no valores das notas

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -22,15 +22,33 @@ console.log ('O número escolhido é:', media);
 if (media >= 10) {
     console.log('APROVADO');
     document.getElementById('resultado').innerHTML += 'TE AMO MIL MILHÕES';
+} else if (media >= 9) {
+    console.log('REPROVADO');
+    document.getElementById('resultado').innerHTML += 'Falta um pouquinho';
 } else if (media >= 8) {
-    console.log('Aprovado');
-    document.getElementById('resultado').innerHTML += 'Tá quase lá.';
-} else if (media >= 5) {
-    console.log('Aprovado');
+    console.log('REPROVADO');
+    document.getElementById('resultado').innerHTML += 'Quase lá.';
+} else if (media >= 7) {
+    console.log('REPROVADO');
     document.getElementById('resultado').innerHTML += 'Pode melhorar.';
+} else if (media >= 6) {
+    console.log('REPROVADO');
+    document.getElementById('resultado').innerHTML += '6 é média e não sou nota.';
+} else if (media >= 5) {
+    console.log('REPROVADO');
+    document.getElementById('resultado').innerHTML += 'Só metade?.';
+} else if (media >= 4) {
+    console.log('REPROVADO');
+    document.getElementById('resultado').innerHTML += 'Pouco para metade.';
+} else if (media >= 3) {
+    console.log('REPROVADO');
+    document.getElementById('resultado').innerHTML += 'Tá errado';
+} else if (media >= 2) {
+    console.log('REPROVADO');
+    document.getElementById('resultado').innerHTML += 'Está querendo mentir para quem?';
 } else {
     console.log('REPROVADO');
-    document.getElementById('resultado').innerHTML += 'Você ama mais, pode votar de novo!';
+    document.getElementById('resultado').innerHTML += 'Não é válido!';
 }
 
 console.log ('Fim do programa');
